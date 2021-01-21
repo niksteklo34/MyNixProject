875446d6e66fa65f1be374de341ba06ef4724550
Nikita <nik123mik456@gmail.com>
2021-01-21 22:29:03 +0300

    Altered main page

--- mainPage.php
+++ mainPage.php
@@ -9,49 +9,69 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>Coffezin</title>
     <style>
+
+        /* Navbar */
+
+        .links .link {
+            font-size: 18px;
+            margin: 0 0 10px 10px;
+            background-color: green;
+            color: white;
+            text-decoration: none;
+            padding: 5px;
+        }
+        .brand {
+            color: white;
+            font-size: 32px;
+        }
+        .navbar {
+            background-color: olive;
+        }
+        hr {
+            height: 5px;
+            background-color: black;
+            margin: 0;
+        }
+
+        /* Container */
+
         .container {
-            margin-top: 200px;
+            margin-top: 75px;
             border: 5px black solid;
             border-radius: 15px;
             height: 575px;
             width: 800px;
             background-color: #808000;
         }
-        .links {
-            margin-top: 15px;
-        }
-        .links a {
-            background-color: green;
-            color: white;
-            text-decoration: none;
-            padding: 5px;
-        }
         .text {
             margin-top: 75px;
             color: white;
-        }
-        .catalog {
-            margin-top: 30px;
         }
         .form-find {
             max-width: 300px;
             margin: 25px auto 25px;
         }
+        .catalog {
+            margin-top: 30px;
+        }
     </style>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="links container-md">
+            <a class="brand navbar-brand" href="#">Coffezin</a>
+            <a class="link btn btn-success" href="">Домой</a>
+        </div>
+    </nav>
+    <hr>
     <div class="container">
         <div class="mt-4">
-            <div class="links text-left">
-                <a class="btn" href="">Войти</a>&nbsp&nbsp
-                <a class="btn" href="">Зарегистрироваться</a><br>
-            </div>
             <div class="text text-center">
                 <H1>Добро пожаловать на наш сайт!</H1><br><br>
                 <form class="text-center" action="" method="post">
                     <div class="test form-floating mb-3">
-                        <label for="floatingInput">Я ищу...</label>
-                        <input type="email" class="form-find form-control" id="floatingInput" placeholder="Название товара..." name="name">
+                        <label for="search">Я ищу...</label>
+                        <input type="text" class="form-find form-control" id="search" placeholder="Название товара..." name="search">
                     </div>
                     <button type="button" class="btn btn-primary">Искать</button><br><br>
                 </form>
