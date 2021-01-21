4a36ba6486b1dc0f17f1969f8187ada355023a78
Nikita <nik123mik456@gmail.com>
2021-01-21 22:12:26 +0300

    Altered catalog page

--- catalog.php
+++ catalog.php
@@ -9,24 +9,50 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>Coffezin</title>
     <style>
-        li {
-            list-style: none;
-        }
-        .links {
-            margin-top: 15px;
-        }
-        .links a {
+
+        /* Navbar */
+
+        .links .link {
+            font-size: 18px;
+            margin: 0 0 10px 10px;
             background-color: green;
             color: white;
             text-decoration: none;
             padding: 5px;
         }
+        .brand {
+            color: white;
+            font-size: 32px;
+        }
+        .navbar {
+            background-color: olive;
+        }
+        hr {
+            height: 5px;
+            background-color: black;
+            margin: 0;
+        }
+
+        /* Container */
+
+        li {
+            list-style: none;
+        }
+        .container {
+            margin-top: 75px;
+        }
         .product-item {
             background-color: #808000;
             padding: 0 25px;
-            margin-top: 20px;
+            margin-top: 25px;
             border: 5px black solid;
             border-radius: 15px;
+        }
+        .product-name {
+            text-align: center;
+            padding: 10px;
+            color: white;
+            font-size: 24px;
         }
         .product-img {
             background-color: white;
@@ -54,12 +80,6 @@
             color: black;
             font-size: 20px;
         }
-        .product-name {
-            text-align: center;
-            padding: 10px;
-            color: white;
-            font-size: 24px;
-        }
         .btn {
             margin-top: 10px;
             margin-bottom: 10px;
@@ -67,58 +87,61 @@
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="links">
-            <a class="btn btn-success" href="">Домой</a>
-            <a class="btn btn-success" href="">В корзину</a>
+    <nav class="navbar navbar-expand-lg">
+        <div class="links container-md">
+            <a class="brand navbar-brand" href="#">Coffezin</a>
+            <a class="link btn btn-success" href="">Домой</a>
         </div>
+    </nav>
+    <hr>
+    <div class="container">
         <ul>
             <li>
                 <div class="product-item">
                     <div class="row">
-                        <div class="product-name col-lg-12 col-md-12">Кофемолка POLARIS PCG 0815</div>
-                        <div class="product-img col-lg-3 col-md-3">Много текста вместо картинки и тд</div>
-                        <div class="product-desc col-lg-9 col-md-9">Высококачественная кофемолка от компании GORENJE отличается неверостно простым и удобным использованием. Она оснащена мотором мощностью 150 Вт, которой будет достаточно для быстрой и эффективной работы. Он характеризуется стабильной и безопасной работой. Устройство оснащено прочным корпусом с продуманным дизайном. В него вы сможете засыпать до 60 г зернового кофе. Также в нем установлен острый одинарный нож, обеспечивающий эффективный помол.</div>
-                        <div class="price col-lg-3 col-md-3">999 грн</div>
+                        <div class="product-name col-lg-12 col-md-12 col-12">Кофемолка POLARIS PCG 0815</div>
+                        <div class="product-img col-lg-3 col-md-3 col-3">Много текста вместо картинки и тд</div>
+                        <div class="product-desc col-lg-9 col-md-9 col-9">Высококачественная кофемолка от компании GORENJE отличается неверостно простым и удобным использованием. Она оснащена мотором мощностью 150 Вт, которой будет достаточно для быстрой и эффективной работы. Он характеризуется стабильной и безопасной работой. Устройство оснащено прочным корпусом с продуманным дизайном. В него вы сможете засыпать до 60 г зернового кофе. Также в нем установлен острый одинарный нож, обеспечивающий эффективный помол.</div>
+                        <div class="price col-lg-3 col-md-3 ">999 грн</div>
                         <div class="availability-yes col-lg-6 col-md-6">Есть в наличии</div>
-                        <button class="btn btn-primary col-lg-3 col-md-3">В корзину</button>
+                        <button class="btn btn-primary col-lg-3 col-md-3 col-3">В корзину</button>
                     </div>
                 </div>
             </li>
             <li>
                 <div class="product-item">
                     <div class="row">
-                        <div class="product-name col-lg-12 col-md-12">Кофемолка GORENJE SMK 150 W</div>
-                        <div class="product-img col-lg-3 col-md-3">Много текста вместо картинки и тд</div>
-                        <div class="product-desc col-lg-9 col-md-9">Кофемолка мощностью 150 Вт с чашей вместительностью до 36 г кофейных зерен. Нож из нержавеющей стали быстро и качественно измельчит кофе и другие подобные продукты, к примеру, превратит сахар в сахарную пудру. Есть таймер и лампочка-индикатор. Прозрачная крышка кофемолки GORENJE SMK 150 W позволит наблюдать за процессом перемалывания. Для безопасности предусмотрена защита от перегрева.</div>
+                        <div class="product-name col-lg-12 col-md-12 col-12">Кофемолка GORENJE SMK 150 W</div>
+                        <div class="product-img col-lg-3 col-md-3 col-3">Много текста вместо картинки и тд</div>
+                        <div class="product-desc col-lg-9 col-md-9 col-9">Кофемолка мощностью 150 Вт с чашей вместительностью до 36 г кофейных зерен. Нож из нержавеющей стали быстро и качественно измельчит кофе и другие подобные продукты, к примеру, превратит сахар в сахарную пудру. Есть таймер и лампочка-индикатор. Прозрачная крышка кофемолки GORENJE SMK 150 W позволит наблюдать за процессом перемалывания. Для безопасности предусмотрена защита от перегрева.</div>
                         <div class="price col-lg-3 col-md-3">799 грн</div>
                         <div class="availability-no col-lg-6 col-md-6">Нет в наличии</div>
-                        <button class="btn btn-primary col-lg-3 col-md-3">В корзину</button>
+                        <button class="btn btn-primary col-lg-3 col-md-3 col-3">В корзину</button>
                     </div>
                 </div>
             </li>
             <li>
                 <div class="product-item">
                     <div class="row">
-                        <div class="product-name col-lg-12 col-md-12">Кофемолка GORENJE SMK150E (CG9139-GS)</div>
-                        <div class="product-img col-lg-3 col-md-3">Много текста вместо картинки и тд</div>
-                        <div class="product-desc col-lg-9 col-md-9">Надежная кофемолка электрического типа. Модель POLARIS PCG 0815 станет отличной помощницей для хозяек. Устройство с легкостью справится с перемалыванием сахара в пудру, кофейных зерен, разнообразных круп и трав. Кофемолка обладает мощностью 150 Вт. Корпус и нож изделия выполнены из нержавеющей стали высокого качества.</div>
+                        <div class="product-name col-lg-12 col-md-12 col-9">Кофемолка GORENJE SMK150E (CG9139-GS)</div>
+                        <div class="product-img col-lg-3 col-md-3 col-3">Много текста вместо картинки и тд</div>
+                        <div class="product-desc col-lg-9 col-md-9 col-9">Надежная кофемолка электрического типа. Модель POLARIS PCG 0815 станет отличной помощницей для хозяек. Устройство с легкостью справится с перемалыванием сахара в пудру, кофейных зерен, разнообразных круп и трав. Кофемолка обладает мощностью 150 Вт. Корпус и нож изделия выполнены из нержавеющей стали высокого качества.</div>
                         <div class="price col-lg-3 col-md-3">1199 грн</div>
                         <div class="availability-yes col-lg-6 col-md-6">Есть в наличии</div>
-                        <button class="btn btn-primary col-lg-3 col-md-3">В корзину</button>
+                        <button class="btn btn-primary col-lg-3 col-md-3 col-3">В корзину</button>
                     </div>
                 </div>
             </li>
             <li>
                 <div class="product-item">
                     <div class="row">
-                        <div class="product-name col-lg-12 col-md-12">Кофемолка ARIETE 3016</div>
-                        <div class="product-img col-lg-3 col-md-3">Много текста вместо картинки и тд</div>
-                        <div class="product-desc col-lg-9 col-md-9">КЛЮЧЕВЫЕ ХАРАКТЕРИСТИКИ
+                        <div class="product-name col-lg-12 col-md-12 col-12">Кофемолка ARIETE 3016</div>
+                        <div class="product-img col-lg-3 col-md-3 col-3">Много текста вместо картинки и тд</div>
+                        <div class="product-desc col-lg-9 col-md-9 col-9">КЛЮЧЕВЫЕ ХАРАКТЕРИСТИКИ
                             Измельчить кофейные зерна для любимого бодрящего напитка вам поможет роторная кофемолка TEFAL GT110838 мощностью 180 Вт. Острые ножи тщательно перемалывают зерна, поэтому ваш кофе всегда будет вкусным и ароматным. Кроме того, с помощью этого прибора можно измельчить орехи, специи, кусочки твердых фруктов. Вместимость кофемолки составляет 50 г.</div>
                         <div class="price col-lg-3 col-md-3">2599 грн</div>
                         <div class="availability-yes col-lg-6 col-md-6">Есть в наличии</div>
-                        <button class="btn btn-primary col-lg-3 col-md-3">В корзину</button>
+                        <button class="btn btn-primary col-lg-3 col-md-3 col-3">В корзину</button>
                     </div>
                 </div>
             </li>
