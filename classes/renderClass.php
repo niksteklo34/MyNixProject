587f0d86aa65e241b62ccac38da470ac27a9a850
Nikita <nik123mik456@gmail.com>
2021-01-26 14:41:53 +0200

    Add exceptions for layout and content page

--- classes/renderClass.php
+++ classes/renderClass.php
@@ -12,17 +12,36 @@ class renderClass
         require_once __DIR__ . $layouts . "limbs/header.php";
         $header = ob_get_clean();
 
-        // Подключение content
-        ob_start();
-        require_once __DIR__ . $templates . $template .".php";
-        $content = ob_get_clean();
-
         // Подключение footer
         ob_start();
         require_once __DIR__ . $layouts . "/limbs/footer.php";
         $footer = ob_get_clean();
 
-        // Подключение шаблона страницы
-        require_once __DIR__ . $layouts . $layout . ".php";
+        try {
+
+            // Подключение content
+            if (file_exists(__DIR__ . $templates . $template . ".php")) {
+                ob_start();
+                require_once __DIR__ . $templates . $template . ".php";
+                $content = ob_get_clean();
+            } else {
+                throw new TemplateRendererExceprion('Такого контента нет<br>');
+            }
+        } catch (TemplateRendererExceprion $errors) {
+            echo $errors->getMessage();
+        }
+
+        try {
+
+            // Подключение шаблона страницы
+            if (file_exists(__DIR__ . $layouts . $layout . ".php")){
+                require_once __DIR__ . $layouts . $layout . ".php";
+            } else {
+                throw new LayoutRendererExceprion('Такой страницы layout нет');
+            }
+        } catch (LayoutRendererExceprion $errors) {
+            echo $errors->getMessage();
+        }
+
     }
 }
