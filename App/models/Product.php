896c6a52a91dfdc5518683e1e6d20b366c36b80d
Nikita <nik123mik456@gmail.com>
2021-01-28 16:00:58 +0200

    Fix name variable

--- App/models/Product.php
+++ App/models/Product.php
@@ -11,12 +11,12 @@ class Product
         $DB = 'products.txt';
         $productsFromDB = file_get_contents($DB, true);
         $products = explode('==========', $productsFromDB);
-        $arrayAllData = [];
+        $productData = [];
         foreach ($products as $product) {
             $productProperties = explode(';;;', $product);
             $allValues = (object) [ 'id' => $productProperties[0], 'name' => $productProperties[1], 'img' => $productProperties[2], 'desc' => $productProperties[3], 'price' => $productProperties[4], 'status' => $productProperties[5]];
-            array_push($arrayAllData, $allValues);
+            array_push($productData, $allValues);
         }
-        return $arrayAllData;
+        return $productData;
     }
 }
